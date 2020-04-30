0ce9d5d264f5328ae225edf80fc457c4ed45d940
mylo8 <vargaandris97@gmail.com>
2020-04-30 11:35:13 +0200

    Bejelentkezés, Session-be írása a felhasználónévnek, vezetéknévnek és keresztnévnek.

--- templates/login.tpl.php
+++ templates/login.tpl.php
@@ -1,3 +1,30 @@
+<?php
+    session_start();
+    if(isset($_POST['name']) && isset($_POST['psw'])) {
+        try {
+            // Kapcsolódás
+            $dbh = new PDO('mysql:host=localhost;dbname=menhely_db', 'root', '',
+                            array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+            $dbh->query('SET NAMES utf8 COLLATE utf8_hungarian_ci');
+            
+            // Felhsználó keresése
+            $sqlSelect = "select login, veznev, kernev from felhasznalok where login = :bejelentkezes and jelszo = sha1(:jelszo)";
+            $sth = $dbh->prepare($sqlSelect);
+            $sth->execute(array(':bejelentkezes' => $_POST['name'], ':jelszo' => $_POST['psw']));
+            if($row = $sth->fetch(PDO::FETCH_ASSOC))
+            {
+                $_SESSION['login'] = $row['login'];
+                $_SESSION['veznev'] = $row['veznev'];
+                $_SESSION['kernev'] = $row['kernev'];
+            }
+
+        }
+        catch (PDOException $e) {
+            echo "Hiba: ".$e->getMessage();
+        }      
+    }
+?>
+
 <form method="post">
     <label for="name">Név:</label>
     <input type="text" name="name" placeholder="Név" required>
